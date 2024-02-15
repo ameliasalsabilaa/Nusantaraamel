25b8ed5bf30ac745c58f1e888366e8b4735c3cb9
ameliasalsabilaa <xameliasalsabilax@gmail.com>
2024-02-15 23:15:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// carousel
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
+
+// navbar
+var nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 700) {
+    nav.classList.add("bg-dark", "shadow");
+  } else {
+    navbar.classList.remove("bg-dark", "shadow");
+  }
+});
